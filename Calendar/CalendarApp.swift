9f6f908ea675e934623c707748e0253ac2470be5
Omar Sánchez <me@omaredu.com>
2025-03-28 17:57:19 -0600

    Initital Firebase installation

--- Calendar/CalendarApp.swift
+++ Calendar/CalendarApp.swift
@@ -5,19 +5,33 @@
 //  Created by Omar Sánchez on 28/03/25.
 //
 
-import SwiftUI
+import FirebaseCore
 import SwiftData
+import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication
+            .LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct CalendarApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-//            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+        let schema = Schema([])
+        let modelConfiguration = ModelConfiguration(
+            schema: schema, isStoredInMemoryOnly: false)
 
         do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+            return try ModelContainer(
+                for: schema, configurations: [modelConfiguration])
         } catch {
             fatalError("Could not create ModelContainer: \(error)")
         }
